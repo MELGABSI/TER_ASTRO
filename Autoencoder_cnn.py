37d4e751073427ce559865bc9aeeb16e5cc5fea8
TALEB34 <35459319+TALEB34@users.noreply.github.com>
2019-04-28 02:55:30 +0200

    petit changement

--- Autoencoder_cnn.py
+++ Autoencoder_cnn.py
@@ -9,7 +9,7 @@ import pandas as pd
 import numpy as np
 from keras.models import Sequential
 from keras.layers import LSTM, Input, Conv1D, MaxPooling1D, Flatten,UpSampling1D
-from keras.layers import Dense, Reshape
+from keras.layers import Dense, Reshape, Masking
 from keras.layers import RepeatVector
 from keras.layers import TimeDistributed
 from keras.utils import plot_model
@@ -22,7 +22,7 @@ import keras.callbacks
 # In[3]:
 
 
-tab = pd.read_csv("tab.csv", sep=";")
+tab = pd.read_csv("Data/table.csv", sep=";")
 
 
 # In[4]:
@@ -31,22 +31,27 @@ tab = pd.read_csv("tab.csv", sep=";")
 t = tab.values
 min_max_scaler = preprocessing.MinMaxScaler()
 t = min_max_scaler.fit_transform(t)
+mask = tab
+mask[mask!=0]=1
+mask = mask.values
 
+s = mask * t 
 
 # In[11]:
 
 
-x_train = t.reshape((len(t), 1095, 1))
-y_train = t.reshape((len(t), 1095, 1))
+x_train = s.reshape((len(s), 1095, 1))
+y_train = s.reshape((len(s), 1095, 1))
 
 
 # In[8]:
 
 
-input_sig = Input(batch_shape=(24,1095,1))
-x0 = Conv1D(512,5, activation='relu', padding='valid')(input_sig)
+input_sig = Input(batch_shape=(72,1095,1))
+out = TimeDistributed(Masking(mask_value=0))(input_sig)
+x0 = Conv1D(512,5, activation='relu', padding='valid')(out)
 x1_ = MaxPooling1D(2)(x0)
-x = Conv1D(128,5, activation='relu', padding='valid')(input_sig)
+x = Conv1D(128,5, activation='relu', padding='valid')(x1_)
 x1 = MaxPooling1D(2)(x)
 x2 = Conv1D(32,5, activation='relu', padding='valid')(x1)
 x3 = MaxPooling1D(2)(x2)
@@ -74,14 +79,15 @@ autoencoder.compile(optimizer='adam', loss='mse')
 
 
 autoencoder.summary()
-
+encoder = Model(input_sig, encoded)
+features = encoder.predict(x_train, batch_size=72)
+np.save("Data/feat.npy", features)
 
 # In[10]:
 
 
 autoencoder.fit(x_train,y_train,
-                      batch_size=24,
-                      epochs=10,
-                      validation_split=0.2,
+                      batch_size=72,
+                      epochs=3,
                       verbose=1)
 
